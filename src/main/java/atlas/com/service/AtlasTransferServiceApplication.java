35e67120e3a22776dd82e1521140385c6cb47521
Guilherme Matte <guiguimatte@gmail.com>
2026-03-07 19:02:33 -0300

    Springboot initialized

---
+++ src/main/java/atlas/com/service/AtlasTransferServiceApplication.java
@@ -0,0 +1,13 @@
+package atlas.com.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtlasTransferServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtlasTransferServiceApplication.class, args);
+	}
+
+}
