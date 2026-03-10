4eeecdc33b66e892103383f231d873712f8d3419
Guilherme Matte <guiguimatte@gmail.com>
2026-03-10 00:20:35 -0300

    criado construtor base para o cartão

--- src/main/java/atlas/com/service/models/Card.java
+++ src/main/java/atlas/com/service/models/Card.java
@@ -3,22 +3,27 @@ package atlas.com.service.models;
 import atlas.com.service.enums.CardLevelEnum;
 import atlas.com.service.enums.CardStatusEnum;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.YearMonth;
 
 @Entity
 @Table(name = "cards")
+@Getter
+@Setter
 public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long userCPF;
+    private String userCPF;
 
     private String PAN;
     private String CVV;
     private String PIN;
-    private LocalDate expirationDate;
+    private YearMonth expirationDate;
+
     private String brand;
 
     private String cardholderName;
@@ -35,9 +40,39 @@ public class Card {
     @Enumerated(EnumType.STRING)
     private CardLevelEnum level;
 
+    private boolean international;
 
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
 
-    private boolean international;
+
+    public Card(Long id,
+                String userCPF,
+                String PAN,
+                String CVV,
+                String PIN,
+                YearMonth expirationDate,
+                String brand,
+                String cardholderName,
+                String deviceId,
+                int failedAttempts,
+                CardStatusEnum status,
+                CardLevelEnum level,
+                boolean international
+    ) {
+        this.id = id;
+        this.userCPF = userCPF;
+        this.expirationDate = YearMonth.now().plusMonths(30);
+        this.brand = brand;
+        this.cardholderName = cardholderName;
+        this.deviceId = deviceId;
+        this.failedAttempts = 0;
+        this.status = status;
+        this.level = level;
+        this.international = international;
+
+        LocalDateTime date = LocalDateTime.now();
+        this.createdAt = date;
+        this.updatedAt = date;
+    }
 }
